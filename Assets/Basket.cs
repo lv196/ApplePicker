e0c6f61b387c7571f45e773c03303e469482eb84
lv196 <lv196@scarletmail.rutgers.edu>
2020-02-17 20:48:37 -0500

    Apple Drop Notification

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; //This line enables use of uGUI features
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    private void Start()
+    {
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        //Get the text component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+        //Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
     void Update(){
         //Get the current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition;
@@ -27,8 +41,13 @@ public class Basket : MonoBehaviour
         if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            //Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+            //Add points for catching the apple
+            score += 100;
+            //Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
         }
     }
-
-
 }
